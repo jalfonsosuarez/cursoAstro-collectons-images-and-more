5c488c94c63101bc2e5aeb65c233e9589c59994f
= <=>
2024-07-28 14:59:54 +0200

    Changes for XML data

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,6 +2,10 @@ import rss from "@astrojs/rss";
 
 import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
+import MarkdownIt from "markdown-it/index.js";
+import sanitizeHtml from "sanitize-html";
+
+const parser = new MarkdownIt();
 
 export const GET: APIRoute = async ({ params, request, site }) => {
   const blogPost = await getCollection("blog");
@@ -12,12 +16,29 @@ export const GET: APIRoute = async ({ params, request, site }) => {
     title: "Un blog realizado con Astro",
 
     description: "Mis aventuras con Astro",
+
+    xmlns: {
+      media: "http://search.yahoo.com/mrss/",
+    },
+
     site: site ?? "",
-    items: blogPost.map(({ data, slug }) => ({
+    items: blogPost.map(({ data, slug, body }) => ({
       title: data.title,
       pubDate: data.date,
       description: data.description,
       link: `/posts/${slug}`,
+
+      content: sanitizeHtml(parser.render(body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
+
+      customData: `<media:content
+          type="image/${data.image.format === "jpg" ? "jpeg" : "png"}"
+          width="${data.image.width}"
+          height="${data.image.height}"
+          medium="image"
+          url="${site + data.image.src}" />
+      `,
     })),
     customData: `<language>es-ES</language>`,
   });
